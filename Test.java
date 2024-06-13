9b6c77db37eaac89effd4da7297d9362aa4f44b7
MounikaNammina <namminamounika@gmail.com>
2024-06-13 18:14:52 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,14 @@
 public class Test {
     public static void main(String[] args) {
         File file=new File(args[0]);
-    }
+        if (!isInSecureDir(file)) {
+              throw new IllegalArgumentException();
+            }
+            String canonicalPath = file.getCanonicalPath();
+        if (!canonicalPath.equals("/img/java/file1.txt") &&
+            !canonicalPath.equals("/img/java/file2.txt")) {
+           // Invalid file; handle error
+        }
 
-}
+        FileInputStream fis = new FileInputStream(f);
+    }
