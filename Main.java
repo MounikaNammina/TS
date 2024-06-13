a7bf3c023790065bc8e3f6742048e6b19436f266
MounikaNammina <namminamounika@gmail.com>
2024-06-13 17:57:10 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,11 @@ class Main
   {
     public static void main(String[] args)
     {
+     
+     String username ="Nammina";
+       String password ="password";
+      
+      
       System.out.println("Hello World");
     }
   }
